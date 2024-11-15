14c50d3dd55bf23fa99b0f85afb511b220958a6c
John-Roncal <johnroncal.19.jarc@gmail.com>
2024-11-14 22:58:07 -0500

    Se agregó un CrossOrigin en el endpoint de login para que el frontend pueda acceder.

--- src/main/java/com/backend/LosPostes/auth/AuthController.java
+++ src/main/java/com/backend/LosPostes/auth/AuthController.java
@@ -1,6 +1,7 @@
 package com.backend.LosPostes.auth;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AuthController {
     
